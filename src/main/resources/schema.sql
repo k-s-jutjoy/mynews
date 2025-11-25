ee8859627ba2b467badeceecafc4783918896108
k-s-jutjoy <kos.saito.jutjoy@gmail.com>
2025-11-25 20:51:13 +0900

    sqlのプッシュ

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,20 @@
--- Database: mynews
+-- Table: public.profiles
 
--- DROP DATABASE IF EXISTS mynews;
+-- DROP TABLE IF EXISTS public.profiles;
 
-CREATE DATABASE mynews
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Japanese_Japan.932'
-    LC_CTYPE = 'Japanese_Japan.932'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+CREATE TABLE IF NOT EXISTS public.profiles
+(
+    id integer NOT NULL DEFAULT nextval('profiles_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    gender character varying(255) COLLATE pg_catalog."default",
+    hobby character varying(255) COLLATE pg_catalog."default",
+    introduction character varying(255) COLLATE pg_catalog."default",
+    registered_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_date timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT profiles_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.profiles
+    OWNER to postgres;
